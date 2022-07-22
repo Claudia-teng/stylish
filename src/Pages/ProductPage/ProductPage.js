556ba58c9f4237a92b9c2911f6583f3695987513
Claudia-teng <sydeng102@gmail.com>
2022-07-22 15:05:50 +0800

    feat: add product details

--- src/Pages/ProductPage/ProductPage.js
+++ src/Pages/ProductPage/ProductPage.js
@@ -28,23 +28,35 @@ function ProductPage() {
             <h2>TWD.{product.price}</h2>
             <div className={styles.color}>
               <span>顏色</span>
+              <div className={styles.colorBoxes}>
+                {product?.colors?.map((color, i) => {
+                  return <div key={i} style={{ backgroundColor: `#${color.code}` }}></div>;
+                })}
+              </div>
             </div>
             <div className={styles.size}>
               <span>尺寸</span>
+              <div className={styles.sizesBoxes}>
+                {product?.sizes?.map((size, i) => {
+                  return <div key={i}>{size}</div>;
+                })}
+              </div>
             </div>
             <div className={styles.count}>
               <span>數量</span>
             </div>
             <button>加入購物車</button>
-            <h3>111</h3>
-            <h3>111</h3>
-            <h3>111</h3>
+            <h3>{product.note}</h3>
+            <h3 className={styles.texture}>{product.texture}</h3>
+            <h3 className={styles.description} dangerouslySetInnerHTML={{ __html: product.description }}></h3>
+            <h3>清洗：{product.wash}</h3>
+            <h3>產地：{product.place}</h3>
           </div>
         </div>
-        <hr />
-        <p>{product.description}</p>
-        {/* <img alt="other_image1" src={product.images[0]} />
-        <img alt="other_image2" src={product.images[1]} /> */}
+        <div className={styles.moreDetail}>更多產品資訊</div>
+        <p>{product.story}</p>
+        <img alt="other_image1" src={product?.images?.length ? product.images[0] : null} />
+        <img alt="other_image2" src={product?.images?.length ? product.images[1] : null} />
       </div>
     </>
   );
